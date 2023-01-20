0a8a5d185997714bf29de2098be7631095f86f20
Jamie Taffurelli <jltaffurelli@outlook.com>
2023-01-20 00:19:12 +0000

    added private link service

--- local.tf
+++ local.tf
@@ -1,3 +1,4 @@
 locals {
-  deployed_backend_address_pools = [for k in azurerm_lb_backend_address_pool.backend_address_pool : k.name]
+  deployed_backend_address_pools      = [for k in azurerm_lb_backend_address_pool.backend_address_pool : k.name]
+  deployed_frontend_ip_configurations = [for k in azurerm_lb.load_balancer.frontend_ip_configuration : k.name]
 }
